eadb321a977b15fa50467821429d1110c9abdba0
cuatrotales <jfquinterogu14@gmail.com>
2022-10-10 12:07:12 -0500

    Se realizo una calculadora basica con input submit (se deben cambiar) y todos los botones son funcionales, pero se debe mejorar el aspecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'miniCalculadora';
+  title = 'Mini Calculadora';
+  numeroUno: number = 0;
+  numeroDos: number = 0;
+  result: number = 0;
+  mensaje: string = "";
+
+  sumar():void{
+    this.result=this.numeroUno+this.numeroDos;
+  }
+
+  restar():void{
+    this.result=this.numeroUno-this.numeroDos;
+  }
+
+  multiplicar():void{
+    this.result=this.numeroUno*this.numeroDos;
+  }
+
+  dividir():void{
+    if(this.numeroUno == 0){
+      this.mensaje = "Ahuevado como va a hacer eso ._."
+    } else{
+      this.result = this.numeroUno/this.numeroDos;
+    }
+  }
 }
